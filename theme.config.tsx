475603163dc88c9c1979b95499cb01676afe94c2
Lucemans <luc@lucemans.nl>
2022-07-28 13:39:51 +0200

    Introduce Nextra

---
+++ theme.config.tsx
@@ -0,0 +1,76 @@
+export default {
+    github: 'https://github.com/ensdomains/docs-v2',
+    docsRepositoryBase: 'https://github.com/ensdomains/docs-v2/blob/main',
+    titleSuffix: ' — ENS Documentation',
+    logo: (
+        <>
+            <span className="mr-2 font-extrabold hidden md:inline">ENS</span>
+            <span className="text-gray-600 font-normal hidden md:inline">
+                Docs
+            </span>
+        </>
+    ),
+    head: (
+        <>
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+            <meta httpEquiv="Content-Language" content="en" />
+            <meta name="description" content="ENS Documentation" />
+            <meta name="og:description" content="ENS Documentation" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta
+                name="twitter:image"
+                content="https://nextra.vercel.app/og.png"
+            />
+            <meta name="twitter:site:domain" content="docs.ens.domains" />
+            <meta name="twitter:url" content="https://docs.ens.domains" />
+            <meta name="og:title" content="ENS Domains Documentation" />
+            <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+            <meta name="apple-mobile-web-app-title" content="Nextra" />
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-icon-180x180.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="192x192"
+                href="/android-icon-192x192.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="96x96"
+                href="/favicon-96x96.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+            />
+            <meta
+                name="msapplication-TileImage"
+                content="/ms-icon-144x144.png"
+            />
+        </>
+    ),
+    search: true,
+    prevLinks: true,
+    nextLinks: true,
+    footer: true,
+    footerEditLink: 'Edit this page on GitHub',
+    footerText: <>GPLv3 {new Date().getFullYear()} © ENS Labs</>,
+    unstable_faviconGlyph: 'X',
+};
