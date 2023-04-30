79001afe0396f46280ec670f67783813c96ce2d0
Luc <luc@lucemans.nl>
2023-04-30 20:27:43 +0000

    Bump Dark Mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,45 +1,47 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
-const modeScript = `
-  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+// const modeScript = `
+//   let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
 
-  updateMode()
-  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
-  window.addEventListener('storage', updateModeWithoutTransitions)
+//   updateMode()
+//   darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+//   window.addEventListener('storage', updateModeWithoutTransitions)
 
-  function updateMode() {
-    let isSystemDarkMode = darkModeMediaQuery.matches
-    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+//   function updateMode() {
+//     let isSystemDarkMode = darkModeMediaQuery.matches
+//     let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
 
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
+//     if (isDarkMode) {
+//       document.documentElement.classList.add('dark')
+//     } else {
+//       document.documentElement.classList.remove('dark')
+//     }
 
-    if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode
-    }
-  }
+//     if (isDarkMode === isSystemDarkMode) {
+//       delete window.localStorage.isDarkMode
+//     }
+//   }
 
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
-    window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
-  }
+//   function disableTransitionsTemporarily() {
+//     document.documentElement.classList.add('[&_*]:!transition-none')
+//     window.setTimeout(() => {
+//       document.documentElement.classList.remove('[&_*]:!transition-none')
+//     }, 0)
+//   }
 
-  function updateModeWithoutTransitions() {
-    disableTransitionsTemporarily()
-    updateMode()
-  }
-`;
+//   function updateModeWithoutTransitions() {
+//     disableTransitionsTemporarily()
+//     updateMode()
+//   }
+// `;
 
 const Document = () => {
+    window.localStorage.isDarkMode = 'false';
+
     return (
         <Html lang="en">
             <Head>
-                <script dangerouslySetInnerHTML={{ __html: modeScript }} />
+                {/* <script dangerouslySetInnerHTML={{ __html: modeScript }} /> */}
                 <meta name="theme-color" content="#5298FF"></meta>
             </Head>
             <body className="bg-[#f7f7f7] antialiased dark:bg-zinc-900">
