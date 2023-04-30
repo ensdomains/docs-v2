8be6112c4b25ab13187c01cc6a197482ebbc7516
Luc <luc@lucemans.nl>
2023-04-30 20:35:02 +0000

    Bump Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./{src,mdx}/**/*.{js,mjs,jsx,mdx,tsx}'],
-    darkMode: false,
+    darkMode: 'class',
     theme: {
         fontSize: {
             '2xs': ['0.75rem', { lineHeight: '1.25rem' }],
