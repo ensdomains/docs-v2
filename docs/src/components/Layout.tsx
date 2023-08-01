e2dd3af77a531acae8179adfdceb0ed0d596c22e
Luc <luc@lucemans.nl>
2023-08-01 03:14:20 +0000

    Introduce basic LDJson

--- docs/src/components/Layout.tsx
+++ docs/src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import { Article, WithContext } from 'schema-dts';
 
 import { Prose } from '@/components/mdx/Prose';
 import { Footer } from '@/components/navigation/footer/Footer';
@@ -15,12 +16,30 @@ export const Layout: FC<{
     mdxProperties: MdxPageProps;
     snapshotData?: ProposalData;
 }> = ({ children, mdxProperties, snapshotData }) => {
+    const schema: WithContext<Article> = {
+        '@context': 'https://schema.org',
+        '@type': 'Article',
+        headline: mdxProperties.meta.title,
+        // image: mdxProperties.meta.,
+        author: mdxProperties.meta.contributors?.map((author) => ({
+            '@type': 'Person',
+            name: author,
+            url: 'https://github.com/' + author,
+        })),
+    };
+
     return (
         <SectionProvider sections={mdxProperties.sections}>
             <div className="lg:ml-72 xl:ml-80" id="app">
                 <MotionHeader />
                 <div className="relative px-4 pt-14 sm:px-6 lg:px-8">
                     <main className="relative py-16">
+                        <script
+                            type="application/ld+json"
+                            dangerouslySetInnerHTML={{
+                                __html: JSON.stringify(schema),
+                            }}
+                        />
                         <Prose>{children}</Prose>
 
                         <div className="pagedetails_wrapper">
