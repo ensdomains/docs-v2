017fe73d9ae68f79d0aa25fe0fb99a97189d6063
Luc <luc@lucemans.nl>
2023-08-31 02:16:12 +0000

    Update WIP & CCIP Image

--- docs/src/components/wip/WIP.tsx
+++ docs/src/components/wip/WIP.tsx
@@ -1,4 +1,6 @@
-export const WIP = () => {
+import { FC } from 'react';
+
+export const WIP: FC<{ missing?: string[] }> = ({ missing }) => {
     return (
         <div className="not-prose rounded-md border border-yellow-500 bg-yellow-50 p-4">
             <h2 className="text-xl font-bold text-yellow-800">
@@ -8,6 +10,16 @@ export const WIP = () => {
                 🏗️👷This page is still under construction. Check back at a later
                 date to see more!
             </p>
+            {missing && (
+                <div>
+                    <div>This page is still missing:</div>
+                    <ul className="px-4">
+                        {missing.map((m, index) => (
+                            <li key={index}>- {m}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
