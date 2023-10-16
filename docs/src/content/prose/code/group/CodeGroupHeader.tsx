954ababd1c4131cdbfb7d9ca1cc0eaab0eb58415
Luc <luc@lucemans.nl>
2023-10-16 22:04:36 +0000

    Bump

--- docs/src/content/prose/code/group/CodeGroupHeader.tsx
+++ docs/src/content/prose/code/group/CodeGroupHeader.tsx
@@ -19,7 +19,7 @@ export const CodeGroupHeader: FC<{
     }
 
     return (
-        <div className="relative flex min-h-[calc(theme(spacing.12)+1px)] justify-between gap-x-4 border-b-4 border-zinc-300 bg-neutral-50 px-4 dark:border-zinc-800 dark:bg-zinc-800">
+        <div className="relative flex min-h-[calc(theme(spacing.12)+1px)] justify-between gap-x-4 border-b border-black/10 px-4 dark:border-zinc-800 dark:bg-zinc-800">
             {title && (
                 <h3 className="pt-3 text-xs font-semibold text-black dark:text-white">
                     {title}
