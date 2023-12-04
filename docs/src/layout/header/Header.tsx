d64a5e6e8cd586047bf7cd786f6ff0936ab5b46b
Luc <luc@lucemans.nl>
2023-12-04 13:30:53 +0000

    Introduce TLDs Page, Update Styling, Rework Navigation (#27)

--- docs/src/layout/header/Header.tsx
+++ docs/src/layout/header/Header.tsx
@@ -18,6 +18,7 @@ import {
 
 import { MobileNavigation } from '../sidebar/_legacy/MobileNavigation';
 import { MobileSearch, Search } from './search/Search';
+import { SubHeader } from './subheader/SubHeader';
 import { ThemeSwitcher } from './ThemeSwitcher';
 
 export const Header = forwardRef<HTMLDivElement, { className?: string }>(
@@ -33,104 +34,109 @@ export const Header = forwardRef<HTMLDivElement, { className?: string }>(
         const isDao = pathname.match(/\/dao(\/.*)?/);
 
         return (
-            <motion.header
-                ref={reference}
-                className={clsx(
-                    className,
-                    'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-start gap-6 px-4 transition sm:px-6 md:shadow-sm lg:z-30 lg:px-4',
-                    !isInsideMobileNavigation &&
-                        'left-0 backdrop-blur-sm dark:backdrop-blur',
-                    isInsideMobileNavigation
-                        ? 'bg-white dark:bg-zinc-900'
-                        : 'bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]'
-                )}
-                style={
-                    {
-                        '--bg-opacity-light': bgOpacityLight,
-                        '--bg-opacity-dark': bgOpacityDark,
-                    } as any
-                }
-            >
-                <div className="flex items-center gap-2">
-                    <div className="flex items-center gap-5 lg:hidden">
-                        <MobileNavigation />
-                        <Link href="/" aria-label="Home">
-                            <img
-                                src="/ens/primary/ens_logo_primary.svg"
-                                className="h-8"
-                                alt="ENS Logo"
-                                height={'32'}
-                            />
-                        </Link>
-                    </div>
-                    <div className="hidden w-fit lg:flex">
-                        <Link href={isDao ? '/dao' : '/'} aria-label="Home">
-                            <img
-                                src={
-                                    isDao
-                                        ? '/ens/dao/ens_mark_dao.svg'
-                                        : '/ens/primary/ens_mark_primary.svg'
-                                }
-                                className="fill-ens-dao-400 ml-1 h-8"
-                                alt="ENS Logo"
-                                height={'32'}
-                            />
-                        </Link>
-                    </div>
-
-                    <div
-                        className={cx(
-                            'rounded-md px-2 text-xs font-bold text-white',
-                            isDao ? 'bg-ens-dao-400' : 'bg-ens-light-blue-500'
-                        )}
-                    >
-                        ALPHA
-                    </div>
-                </div>
-                <div
+            <>
+                <motion.header
+                    ref={reference}
                     className={clsx(
-                        'absolute inset-x-0 top-full h-px transition',
-                        (isInsideMobileNavigation || !mobileNavIsOpen) &&
-                            'bg-zinc-900/7.5 dark:bg-white/7.5'
+                        className,
+                        'fixed inset-x-0 top-0 z-50 flex h-16 items-center justify-start gap-6 px-4 transition sm:px-6 lg:z-30 lg:px-4',
+                        'bg-white dark:bg-zinc-900'
                     )}
-                />
-                <Search />
-                <div className="h-full w-auto grow"></div>
-                <div className="flex h-full items-center gap-4">
-                    <div className="hidden gap-2 min-[786px]:flex">
-                        <Button
-                            href="https://app.ens.domains?ref=ens-docs-v2"
-                            target="_blank"
-                            variant="primary"
+                    style={
+                        {
+                            '--bg-opacity-light': bgOpacityLight,
+                            '--bg-opacity-dark': bgOpacityDark,
+                        } as any
+                    }
+                >
+                    <div className="flex items-center gap-2">
+                        <div className="flex items-center gap-5 lg:hidden">
+                            <MobileNavigation />
+                            <Link href="/" aria-label="Home">
+                                <img
+                                    src="/ens/primary/ens_logo_primary.svg"
+                                    className="h-8"
+                                    alt="ENS Logo"
+                                    height={'32'}
+                                />
+                            </Link>
+                        </div>
+                        <div className="hidden w-fit lg:flex">
+                            <Link href={isDao ? '/dao' : '/'} aria-label="Home">
+                                <img
+                                    src={
+                                        isDao
+                                            ? '/ens/dao/ens_logo_dao.svg'
+                                            : '/ens/primary/ens_logo_primary.svg'
+                                    }
+                                    className="fill-ens-dao-400 ml-1 h-8"
+                                    alt="ENS Logo"
+                                    height={'32'}
+                                />
+                            </Link>
+                        </div>
+
+                        <div
+                            className={cx(
+                                'rounded-md px-2 text-xs font-bold text-white',
+                                isDao
+                                    ? 'bg-ens-dao-400'
+                                    : 'bg-ens-light-blue-500'
+                            )}
                         >
-                            App
-                        </Button>
-                        {isDao ? (
-                            <Button href="/" variant="primary" className="">
-                                Protocol Docs
-                            </Button>
-                        ) : (
-                            <Button href="/dao" variant="primary" className="">
-                                DAO Docs
-                            </Button>
+                            Documentation
+                        </div>
+                    </div>
+                    <div
+                        className={clsx(
+                            'absolute inset-x-0 top-full h-px transition',
+                            (isInsideMobileNavigation || !mobileNavIsOpen) &&
+                                'bg-zinc-900/7.5 dark:bg-white/7.5'
                         )}
+                    />
+                    <Search />
+                    <div className="h-full w-auto grow"></div>
+                    <div className="flex h-full items-center gap-4">
+                        <div className="hidden gap-2 min-[786px]:flex">
+                            <Button
+                                href="https://app.ens.domains?ref=ens-docs-v2"
+                                target="_blank"
+                                variant="primary"
+                            >
+                                App
+                            </Button>
+                            {isDao ? (
+                                <Button href="/" variant="primary" className="">
+                                    Protocol Docs
+                                </Button>
+                            ) : (
+                                <Button
+                                    href="/dao"
+                                    variant="primary"
+                                    className=""
+                                >
+                                    DAO Docs
+                                </Button>
+                            )}
+                        </div>
+                        <ThemeSwitcher />
+                        <div className="md:dark:bg-white/15 hidden md:block md:h-5 md:w-px md:bg-zinc-900/10" />
+                        <div className="flex gap-4">
+                            <MobileSearch />
+                            <Link
+                                href="https://github.com/ensdomains/docs-v2"
+                                target="_blank"
+                            >
+                                <FaGithub
+                                    size={'1.4rem'}
+                                    className="opacity-90 hover:opacity-100"
+                                />
+                            </Link>
+                        </div>
                     </div>
-                    <ThemeSwitcher />
-                    <div className="md:dark:bg-white/15 hidden md:block md:h-5 md:w-px md:bg-zinc-900/10" />
-                    <div className="flex gap-4">
-                        <MobileSearch />
-                        <Link
-                            href="https://github.com/ensdomains/docs-v2"
-                            target="_blank"
-                        >
-                            <FaGithub
-                                size={'1.4rem'}
-                                className="opacity-90 hover:opacity-100"
-                            />
-                        </Link>
-                    </div>
-                </div>
-            </motion.header>
+                </motion.header>
+                <SubHeader />
+            </>
         );
     }
 );
