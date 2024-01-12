878a0b64fbe3376e14923b849456fc0a05bb2c5b
Jonatan Holmgren <jonatan@jontes.page>
2024-01-12 19:11:59 +0100

    Introduce basic support for tags in frontend

--- docs/src/layout/header/search/lib/fetcher.ts
+++ docs/src/layout/header/search/lib/fetcher.ts
@@ -2,9 +2,10 @@ import { Fetcher } from 'swr';
 
 import { SearchResult } from '../types/result';
 
-export const searchFetcher: Fetcher<SearchResult, { search: string }> = async ({
-    search,
-}) => {
+export const searchFetcher: Fetcher<
+    SearchResult,
+    { search: string; tag: string }
+> = async ({ search, tag }) => {
     // @ts-ignore
     const result = await fetch(
         'https://search.v3x.systems/indexes/ens-docs/search',
@@ -12,8 +13,6 @@ export const searchFetcher: Fetcher<SearchResult, { search: string }> = async ({
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization:
-                    'Bearer b77dacb494c3272351784097847e34c59b22510b30aa0ed662f1e79e1df658a0',
             } as any,
             body: JSON.stringify({
                 q: search,
@@ -25,9 +24,10 @@ export const searchFetcher: Fetcher<SearchResult, { search: string }> = async ({
                     'slug',
                     'description',
                     'authors',
-                    'tags',
+                    'tag',
                     'emoji',
                 ],
+                filter: tag === 'All' ? undefined : 'tag = ' + tag,
                 cropLength: 10,
                 attributesToHighlight: ['content', 'title'],
             }) as any,
