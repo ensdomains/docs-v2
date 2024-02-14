a37799cb28d16108e3ff131cb40935b06493ef3b
Luc <luc@lucemans.nl>
2024-02-14 03:45:42 +0000

    Update Content

--- app/src/components/mdx/EmbedLink.tsx
+++ app/src/components/mdx/EmbedLink.tsx
@@ -7,20 +7,22 @@ export const EmbedLink: FC<{
     href: string;
     tag?: string;
     description: string;
-}> = ({ title, href, description, tag }) => {
+    target?: string;
+}> = ({ title, href, description, target, tag }) => {
     return (
         <Link
             href={href}
-            className="card1 flex items-center gap-1.5 p-6 text-ens-light-text-primary no-underline outline-blue-500 transition-all hover:bg-ens-light-background-secondary/20 hover:outline-2 dark:text-ens-dark-text-primary hover:dark:bg-ens-dark-background-secondary/20"
+            className="card1 text-ens-light-text-primary hover:bg-ens-light-background-secondary/20 dark:text-ens-dark-text-primary hover:dark:bg-ens-dark-background-secondary/20 flex items-center gap-1.5 p-6 no-underline outline-blue-500 transition-all hover:outline-2"
+            target={target}
         >
-            <div className="not-prose flex w-full items-center gap-1.5 text-ens-light-text-primary no-underline transition-all hover:bg-ens-light-background-secondary/20 dark:text-ens-dark-text-primary hover:dark:bg-ens-dark-background-secondary/20">
+            <div className="not-prose text-ens-light-text-primary hover:bg-ens-light-background-secondary/20 dark:text-ens-dark-text-primary hover:dark:bg-ens-dark-background-secondary/20 flex w-full items-center gap-1.5 no-underline transition-all">
                 <span className="flex w-full flex-col">
                     <span className="font-bold">{title}</span>
                     <span className="font-normal">{description}</span>
                 </span>
 
                 {tag && (
-                    <span className="ml-2 rounded-full bg-ens-light-yellow-surface px-2 text-xs text-ens-light-yellow-active dark:bg-ens-dark-yellow-surface dark:text-ens-dark-yellow-active">
+                    <span className="bg-ens-light-yellow-surface text-ens-light-yellow-active dark:bg-ens-dark-yellow-surface dark:text-ens-dark-yellow-active ml-2 rounded-full px-2 text-xs">
                         {tag}
                     </span>
                 )}
