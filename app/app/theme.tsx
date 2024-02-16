263bc2af5265b67918d17ab79289e35cb46e69e6
Luc <luc@lucemans.nl>
2024-02-16 03:39:36 +0000

    Introduce Thorin Connect Modal
    Breaks WalletConnect

--- app/app/theme.tsx
+++ app/app/theme.tsx
@@ -1,22 +1,24 @@
 'use client';
 
 import '@ens-tools/thorin-core';
+import 'webcomponent-qr-code';
 
+import { setupConfig } from '@ens-tools/thorin-core';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import { goerli, mainnet, sepolia } from 'viem/chains';
 import { createConfig, http, WagmiProvider } from 'wagmi';
-import { injected } from 'wagmi/connectors';
 
-const config = createConfig({
+export const config = createConfig({
     chains: [mainnet, goerli, sepolia],
-    connectors: [injected({})],
     transports: {
         [mainnet.id]: http(),
         [goerli.id]: http(),
         [sepolia.id]: http(),
     },
 });
+
+setupConfig(config as any);
 
 declare module 'wagmi' {
     interface Register {
